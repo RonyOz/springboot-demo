a40266fede89805c149bda9fee8ef0d8e4738e7c
Rony Ordoñez <ronyfarido@gmail.com>
2025-04-22 14:06:25 -0500

    Refactor StudentController and JwtService: remove unused imports and clean up code

--- src/main/java/com/example/springboot_demo/controller/StudentController.java
+++ src/main/java/com/example/springboot_demo/controller/StudentController.java
@@ -1,16 +1,11 @@
 package com.example.springboot_demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springboot_demo.dto.StudentDTO;
 import com.example.springboot_demo.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
--- src/main/java/com/example/springboot_demo/util/JwtService.java
+++ src/main/java/com/example/springboot_demo/util/JwtService.java
@@ -4,12 +4,9 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
 import java.util.*;
 import java.util.stream.Collectors;
 
