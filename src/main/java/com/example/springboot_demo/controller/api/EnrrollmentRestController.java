8c25770a71ce87381153fbd67ce404061645f236
Rony Ordoñez <ronyfarido@gmail.com>
2025-04-26 16:20:27 -0500

    Refactor EnrollmentRestController: remove unused imports and clean up code structure

--- src/main/java/com/example/springboot_demo/controller/api/EnrrollmentRestController.java
+++ src/main/java/com/example/springboot_demo/controller/api/EnrrollmentRestController.java
@@ -1,24 +1,16 @@
 package com.example.springboot_demo.controller.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.springboot_demo.dto.CourseDTO;
-import com.example.springboot_demo.dto.EnrollmentDTO;
-import com.example.springboot_demo.dto.StudentDTO;
-import com.example.springboot_demo.entity.Course;
-import com.example.springboot_demo.entity.Student;
-import com.example.springboot_demo.service.CourseService;
-import com.example.springboot_demo.service.EnrollmentService;
-import com.example.springboot_demo.service.StudentService;
-
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot_demo.dto.EnrollmentDTO;
+import com.example.springboot_demo.service.EnrollmentService;
 
 @RestController
 @RequestMapping("/api/v1/enrollments")
